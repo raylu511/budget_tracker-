32b96c38d9e1f5a711e73ba46a731b09776f5ea1
raylu511 <73660317+raylu511@users.noreply.github.com>
2021-08-15 13:18:20 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy 
 
-
+counter = 0
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:8333@localhost/budget_database'
 db=SQLAlchemy(app)
-
 
 
 # Renders index html on startup 
@@ -18,6 +17,7 @@ class Budget(db.Model):
     __tablename__="budget"
     id=db.Column(db.Integer,primary_key=True)
     budgetAmount_=db.Column(db.Integer)
+    expense=db.relationship('Expense')
 
     def __init__(self,budgetAmount_):
         self.budgetAmount_=budgetAmount_
@@ -29,22 +29,27 @@ class Expense(db.Model):
     id=db.Column(db.Integer,primary_key=True)
     eName_=db.Column(db.String(120))
     eCost_=db.Column(db.Integer)
-    
+    budgetId_=db.Column(db.Integer,db.ForeignKey("budget.id"))
 
-    def __init__(self,eName_,eCost_):
+    def __init__(self,eName_,eCost_,budgetId_):
         self.eName_=eName_
         self.eCost_=eCost_
-        
+        self.budgetId_=budgetId_
 
 
 #POST REQUEST
 @app.route("/submit",methods=["POST"])
 def submit():
+
+
     
     if request.method=="POST":     
         #Budget button
         if request.form["budget"] == "budget":
+            global budgetAmount
             budgetAmount=request.form["budget_amount"] 
+            global counter
+            counter+=1
             print(request.form)
             budget=Budget(budgetAmount)
             db.session.add(budget)
@@ -56,10 +61,10 @@ def submit():
         elif request.form["budget"]=="expense":    
             eName=request.form["expense_name"]
             eCost=request.form["expense_amount"]
-            expense=Expense(eName,eCost)
+            expense=Expense(eName,eCost,counter)
             db.session.add(expense)
             db.session.commit()
-            return render_template("budget.html",eName=eName,eCost=eCost)    
+            return render_template("budget.html",eName=eName,eCost=eCost,budgetAmount=budgetAmount)    
     
 
 if __name__ =='__main__':
